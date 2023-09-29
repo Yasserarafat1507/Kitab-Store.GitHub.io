111129a546516114dd3b46371c90cde515073137
yasser arafat <yasserarafat1507@gmail.com>
2023-09-29 13:46:42 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// toggle icon navbar
+let menuIcon =document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// scroll section active link
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll= () => {
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150 ;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(link =>{
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+    });
+    //remove toggle and navbar when scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+//scroll reveal
+ScrollReveal({ 
+    reset: true ,
+    distance: '80px',
+    duration: 2000,
+    dalay: 200
+});
